7ec9d54111e2db1c8f17027076001d454ff18bdc
gdeherdt <gdeherdt@purdue.edu>
2024-09-28 02:27:27 -0400

    Update run.js

--- out/run.js
+++ out/run.js
@@ -1,31 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 //const { execSync } = require('node:child_process');
-import { execSync } from "node:child_process";
-import {Main} from "./Master"
-
+const node_child_process_1 = require("node:child_process");
+require("./Install");
+require("./Master");
 //execSync(Command, { stdio: 'inherit' });
-function runCommand(fname:string){
-    let file:string = __dirname+'/'+fname;
-    const command = `node ${file}`
-    try{
-    execSync(command, { stdio: 'inherit' });
-    console.log(`Successfully ran ${file}`);
-    }catch (error) {
-      console.error(`Failed to run ${file}`)  ;
+function runCommand(fname) {
+    let file = __dirname + '/' + fname;
+    const command = `node ${file}`;
+    try {
+        (0, node_child_process_1.execSync)(command, { stdio: 'inherit' });
+        console.log(`Successfully ran ${file}`);
+    }
+    catch (error) {
+        console.error(`Failed to run ${file}`);
     }
 }
-    function runMaster(urlFile:string){
-       // let file:string = __dirname+`//`+fname;
-        //const command = `node ${file} ${urlFile}`
-       // console.log(`running to test ${urlFile}`);
-        try{
-        //execSync(command, { stdio: 'inherit' });
-        Main(urlFile);
+function runMaster(fname, urlFile) {
+    let file = __dirname + `//` + fname;
+    const command = `node ${file} ${urlFile}`;
+    console.log(`running subprogram ${file} to test ${urlFile}`);
+    try {
+        (0, node_child_process_1.execSync)(command, { stdio: 'inherit' });
         //console.log(`Successfully ran ${urlFile}`);
-        }catch (error) {
-          //console.error(`Failed to run ${urlFile}`) ; 
-        }
-    
- }
+    }
+    catch (error) {
+        //console.error(`Failed to run ${urlFile}`) ; 
+    }
+}
 var n = 0; //root command loc
 //while(n<3){
 //n++;
@@ -34,40 +36,38 @@ var n = 0; //root command loc
 //process.exit(1);
 //}
 //}
-try{
-while (process.argv[n] != __filename){
-    //console.log(`${process.argv[n]}`);
-    n++;
+try {
+    while (process.argv[n] != __filename) {
+        console.log(`${process.argv[n]}`);
+        n++;
+    }
+    console.log(`file is ${process.argv[n]}`);
+    n = n + 1;
+    console.log(`dir is ${__dirname} ${n}`);
 }
-//console.log(`file is ${process.argv[n]}`);
- n=n+1
- //console.log(`dir is ${__dirname} ${n}`);
-} catch(error){
+catch (error) {
     console.error(`Failed to run program`);
-    process.exit(1) ;
 }
-const commandString:string = process.argv[n];
-if (commandString == null){
+const commandString = process.argv[n];
+if (commandString == null) {
     console.error(`Failed to run, invalid arguments`);
-    process.exit(0) ;
+    process.exit(0);
 }
-if (commandString == 'install'){
-    try{
-//execSync(`git clone https://github.com/IAmDarkMeadow/CS45000-ECE46100 `, { stdio: 'inherit' });
-  //  console.log(`install to ${__dirname}`);
-  require("./Install");
-    runCommand('Install.js');
-    }catch(error){
+if (commandString == 'install') {
+    try {
+        //execSync(`git clone https://github.com/IAmDarkMeadow/CS45000-ECE46100 `, { stdio: 'inherit' });
+        //  console.log(`install to ${__dirname}`);
+        runCommand('Install.js');
+    }
+    catch (error) {
         console.error(`Error`);
     }
 }
-else
-if (commandString == 'test'){
+else if (commandString == 'test') {
     runCommand('TestRun.js');
 }
 else {
-    
-    const url:string = process.argv[n];
-    //require("./Master");
-    runMaster(url);
+    const url = process.argv[n];
+    runMaster('Master.js', url);
 }
+//# sourceMappingURL=run.js.map
